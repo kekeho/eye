ce058d4fa666e0792a9d9b2a2ff55a9af43e024a
Hiroki.T <hirodora@me.com>
2021-06-24 16:36:20 +0900

    Initial Commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 Hiroki Takemura (kekeho)
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+function setup() {
+    let canvas = createCanvas(window.innerWidth, window.innerHeight);
+    canvas.parent('app');
+
+    background(100);
+}
+
+
+function draw() {
+
+}
